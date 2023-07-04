99303b23ddd14e0c8893e0356635db97d8378688
deigo-lps <diego.lopes0909@gmail.com>
2023-07-04 13:55:23 -0300

    initial commit

---
+++ instapage.js
@@ -0,0 +1,67 @@
+const urlParams = new URLSearchParams(window.location.search);
+const origin = window.location.pathname.replace("/", "").replace("/", "");
+document.cookie = `offer_id=${origin}; path=/; domain=.buckedup.com;max-age=3600`;
+localStorage.setItem("first_page", origin);
+const orderID = urlParams.get("order_uuid");
+
+//CHANGE FROM HERE UNTILL COMMENT SAYING TO STOP.
+
+//finish - redirect - post
+//finish will complete the order.
+//redirect will redirect to another page (like a modal).
+//post will add to the order without finishing.
+const prodType = "post";
+const prodID = 620;
+const buyButtonIds = ["element-41"];
+const noThanksButtonsIds = [];
+const finishPostRedirect = `https://www.buckedup.com/extension/misc/upsell/complete?${urlParams}`;
+const noThanksRedirect = ``;
+const modalRedirect = `https://select.buckedup.com/?time=001&discount=og&open=this&product0=${prodID}&${urlParams}&first_page=${origin}&bypass_shipping_protection=1`;
+
+//OFFER STEP:
+// Offer Page: lp
+// Upsell 1: us1
+// Upsell 2: us2
+// Downsell: ds1
+// Final page: fu
+
+// ACTION:
+// when user buys on Landing Page: purchase
+// when user buys on Upsell or Downsell or Final page: purchase-us
+// any other action: click
+
+const setDataLayer = (event, action, value) => {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    step_count: "", //lp, us1, us2, us3, ds1, ty
+    page_id: "", //OG-LP-OMO, pegar pelo query da url, passar pra frente.
+    version_id: "", //v1-control, v2-dropdown, v2-modal
+    event: event, //offer_view, interaction
+    action: action, //purchase, purchase-us, click, view_page
+    value: value, //final purchase value
+    transaction_id: orderID,
+  });
+};
+
+const dataLayerStart = () => {
+  setDataLayer((event = ""), (action = ""), (value = 0));
+};
+
+const dataLayerRedirect = () => {
+  setDataLayer((event = ""), (action = ""), (value = 0));
+};
+
+const dataLayerBuy = (data) => {
+  setDataLayer(
+    (event = ""),
+    (action = ""),
+    (value = data.product.price.slice(1)) //dont change
+  );
+};
+
+const dataLayerNoThanks = () => {
+  setDataLayer((event = ""), (action = ""), (value = 0));
+};
+
+//STOP HERE.
+dataLayerStart();
---
+++ scripts.js
@@ -0,0 +1,93 @@
+const toggleButton = (buttons) => {
+  buttons.forEach((buttonID) => {
+    let button = document.getElementById(buttonID);
+    button.classList.toggle("btn-lock");
+    button.toggleAttribute("disabled");
+  });
+};
+
+const fetchProduct = async (productID) => {
+  const response = await fetch(
+    `https://ar5vgv5qw5.execute-api.us-east-1.amazonaws.com/list/${productID}`
+  );
+  const data = await response.json();
+  if (!response.ok) {
+    console.log("Error Fetching API");
+    console.log(data);
+    return null;
+  }
+  return data;
+};
+
+const postApi = async (url, body) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const responseLog = await response.json();
+  if (!response.ok) {
+    alert("There was a problem with your request. Please try again later.");
+    return false;
+  }
+  return responseLog;
+};
+
+const buy = async (data) => {
+  const fetchURL = `https://ar5vgv5qw5.execute-api.us-east-1.amazonaws.com/upsell/${orderID}`;
+  const fetchURLfinal = `https://ar5vgv5qw5.execute-api.us-east-1.amazonaws.com/upsell/${orderID}/finish`;
+
+  const body = { order_uuid: orderID, items: [] };
+  const item = {};
+  item.product_id = prodID;
+  item.price = parseFloat(data.product.price.slice(1));
+  item.quantity = 1;
+  item.options = {};
+  body.items.push(item);
+
+  const response = await postApi(fetchURL, body);
+  console.log(response);
+  if (!response) return;
+  if (prodType === "finish") {
+    const response = await postApi(fetchURLfinal, null);
+    console.log(response);
+    if (!response) return;
+  }
+  dataLayerBuy(data);
+  window.location.href = finishPostRedirect;
+};
+
+let data = null;
+
+if (prodType === "post" || prodType === "finish") {
+  toggleButton(buyButtonIds);
+  window.onload = async () => {
+    data = await fetchProduct(prodID);
+    if (data == null) {
+      alert("There was a problem with your request.");
+      return;
+    }
+    toggleButton(buyButtonIds);
+  };
+  buyButtonIds.forEach((id) => {
+    const btn = document.getElementById(id);
+    btn.addEventListener("click",()=>{buy(data)});
+  });
+} else {
+  buyButtonIds.forEach((id) => {
+    const btn = document.getElementById(id);
+    btn.addEventListener("click", () => {
+      dataLayerRedirect();
+      window.location.href = modalRedirect;
+    });
+  });
+}
+noThanksButtonsIds.forEach((id) => {
+  const btn = document.getElementById(id);
+  btn?.addEventListener("click", () => {
+    dataLayerNoThanks();
+    window.location.href = noThanksRedirect;
+  });
+});
